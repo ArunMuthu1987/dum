cc821a9cc6cc4d6454fdb8699871208434a77f07
ArunMuthu1987 <arun2422611@gmail.com>
2025-02-24 17:32:32 +0530

    Update Test_1.java

--- src/test/java/Fruit/Test_1.java
+++ src/test/java/Fruit/Test_1.java
@@ -4,7 +4,7 @@ public class Test_1 {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("Welcome to Chennai");
 		System.out.println("HI");
 	}
 }
